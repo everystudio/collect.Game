fa3dbac4436401aa69ec2df6a0c72a6704a753bc
watanabe.everystudio <watanabe.everystudio@gmail.com>
2016-09-07 10:41:28 +0900

    GoogleAnalyticsの修正

--- script/GoogleAnalytics.cs
+++ script/GoogleAnalytics.cs
@@ -6,12 +6,12 @@ public class GoogleAnalytics : GoogleAnalyticsBase<GoogleAnalytics> {
 	public override void Initialize ()
 	{
 		base.Initialize ();
-		propertyID_Common = "UA-77286676-7";
-		propertyID_Android = "UA-77286676-5";
-		propertyID_iOS = "UA-77286676-6";
+		propertyID_Common  = "UA-77286676-7";
+		propertyID_Android = "UA-77286676-12";
+		propertyID_iOS     = "UA-77286676-13";
 
-		bundleID ="jp.everystudio.collect.summer";
-		appName = "ナツヤスミ、いりませんか？";
+		bundleID ="jp.everystudio.collect.song";
+		appName = "壊れた声でも希望を唄って";
 		appVersion = "1";
 
 		Invoke ("heartbeat", HEARTBEAT_INTERVAL);
